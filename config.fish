420929a04034cff63a089d9d8b9057a858d42a08
Vicente <33096355+Vicente015@users.noreply.github.com>
2024-04-01 22:58:03 +0300

    code toolbox

--- config.fish
+++ config.fish
@@ -48,8 +48,8 @@ alias gedit='flatpak run org.gnome.TextEditor'
 alias tbox='SHELL=/usr/bin/fish toolbox -r 38 enter'
 alias trun='SHELL=/usr/bin/fish toolbox -r 38 run $argv'
 alias firefox='flatpak-spawn --host firefox'
-alias code='flatpak run com.visualstudio.code'
-alias jrnl='toolbox -r 38 run jrnl'
+#alias code='flatpak run com.visualstudio.code'
+#alias code='toolbox run -c fedora-toolbox-38 /usr/share/code/code &'
 
 # Hide jrnl entries
 abbr --add jrnl " jrnl"
